2537e64e9f4b8aca4a4ef512d419ce18251775b4
unknown <ubanamarcnoev@gmail.com>
2026-07-24 13:25:53 +0800

    7.24.26 v2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
-  base: "/phaurionsolar/", // must match your GitHub repo name exactly
+  base: "/",
 });
